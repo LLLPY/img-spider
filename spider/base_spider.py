d74448e5bbddab028289d97775f659237bf0167a
LLL <2474605919@qq.com>
2022-11-09 23:08:49 +0800

    '更新了base_spider'

--- spider/base_spider.py
+++ spider/base_spider.py
@@ -4,33 +4,37 @@
 import asyncio
 import aiohttp
 import aiofiles
+import requests
 import os
 import sys
+
 sys.path.append(f'..{os.sep}')
 import conf.conf as conf
-
+import conf.model as model
 
 
 class BaseSpider:
 
-    def __init__(self,url):
-        self.url=url
-        
-    
-    async def download_img(self,save_path:str):
+    def __init__(self, keyword: str) -> None:
+        self.keyword = keyword
+
+    def get(self, url, headers=conf.HEADERS):
+        res = requests.get(url, headers=headers, verify=False)
+        if res.status_code != 200:
+            conf.img_spider_logger.warning(f'[{self.__class__.__name__}.get] 响应码不为200!...')
+
+        return res
+
+    async def download_img(self, img_obj: model.Img, save_path: str):
         async with aiohttp.ClientSession() as session:
             aiohttp.ClientTimeout(5)
-            async with session.get(self.url,headers=conf.HEADERS) as res:   
-                print(self.url) 
-                async with aiofiles.open(save_path,'wb') as f:
-                    content=await res.content.read()
+            async with session.get(img_obj.url, headers=conf.HEADERS) as res:
+                async with aiofiles.open(save_path, 'wb') as f:
+                    content = await res.content.read()
                     await f.write(content)
-                    print(content)
-                    print('下载完成...')                
-        
-
+                    print('下载完成...')
 
 
 if __name__ == '__main__':
-    spider=BaseSpider('http://edu.chachaba.com/Uploads/2020-03-20/5e74678a17ab4.jpg')
-    asyncio.run(spider.download_img('1.html'))
+    spider = BaseSpider('http://edu.chachaba.com/Uploads/2020-03-20/5e74678a17ab4.jpg')
+    asyncio.run(spider.download_img('1.jpg'))
